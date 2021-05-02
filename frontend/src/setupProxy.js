dc1541132e4347cdc7710271df8d43fb7b018b7e
rc-li <husky.li.ruicheng@gmail.com>
2021-05-02 01:57:48 -0700

    changes to the quickstart repo so far for project minions

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = app => {
   app.use(
     "/api",
     proxy({
-      target: process.env.REACT_APP_API_HOST || "http://localhost:8000",
+      // target: process.env.REACT_APP_API_HOST || "http://localhost:8081",
+      target: process.env.REACT_APP_API_HOST || "http://plaidproxyserver-env-1.eba-jpkiu7tz.us-west-1.elasticbeanstalk.com",
       changeOrigin: true
     })
   );
